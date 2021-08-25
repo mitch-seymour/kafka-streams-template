342b0516fbb488442d8d31a504e1004b5afad959
Mitch Seymour <mseymour@rsglab.com>
2021-08-24 20:23:56 -0400

    Add config

--- app/src/main/java/com/example/App.java
+++ app/src/main/java/com/example/App.java
@@ -1,9 +1,7 @@
 package com.example;
 
 import java.util.Properties;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 
 class App {
@@ -11,14 +9,8 @@ class App {
     // instantiate the topology
     Topology topology = MyTopology.build();
 
-    // set the required properties for running Kafka Streams
-    Properties config = new Properties();
-    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "dev-group");
-    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-
-    // set some optional properties
-    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    // load the Kafka Streams configuration
+    Properties config = AppConfig.getKafkaStreamsConfig();
 
     // build the topology and start streaming!
     KafkaStreams streams = new KafkaStreams(topology, config);
